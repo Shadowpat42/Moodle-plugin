f9091e3dc983df5f1fd0ce2c78713beb5f609547
Vladimir <djobirov05@mail.ru>
2024-12-07 14:20:52 +0500

    Add database/access.php for Moodle root

---
+++ db/access.php
@@ -0,0 +1,14 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    'local/statistics:view' => [
+        'riskbitmask' => RISK_PERSONAL,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'manager' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW
+        ],
+    ],
+];
